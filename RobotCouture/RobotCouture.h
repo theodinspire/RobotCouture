d495296d18fefdc62edff9fc948db7949e9d051f
Eric T Cormack <bobryuu@gmail.com>
2018-09-28 13:21:02 -0500

    Initial Commit

---
+++ RobotCouture/RobotCouture.h
@@ -0,0 +1,19 @@
+//
+//  RobotCouture.h
+//  RobotCouture
+//
+//  Created by Eric Cormack on 9/28/18.
+//  Copyright © 2018 the Odin Spire. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RobotCouture.
+FOUNDATION_EXPORT double RobotCoutureVersionNumber;
+
+//! Project version string for RobotCouture.
+FOUNDATION_EXPORT const unsigned char RobotCoutureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RobotCouture/PublicHeader.h>
+
+
