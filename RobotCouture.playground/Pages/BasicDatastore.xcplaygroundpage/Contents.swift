05633cfbf040a91bfb1cc4cedd1662116285c6e5
Eric T Cormack <bobryuu@gmail.com>
2018-10-15 16:05:18 -0500

    Clear pod warnings

--- RobotCouture.playground/Pages/BasicDatastore.xcplaygroundpage/Contents.swift
+++ RobotCouture.playground/Pages/BasicDatastore.xcplaygroundpage/Contents.swift
@@ -4,8 +4,4 @@ import Foundation
 
 print(FileManager.default.currentDirectoryPath)
 
-for element in FileManager.default.contentsOfDirectory(atPath: FileManager.default.currentDirectoryPath).sorted() {
-    print(element)
-}
-
 //: [Next](@next)
---
+++ RobotCouture/Extensions/DateFormatter+Default.swift
@@ -0,0 +1,18 @@
+//
+//  DateFormatter+Default.swift
+//  RobotCouture
+//
+//  Created by Eric Cormack on 10/15/18.
+//  Copyright © 2018 the Odin Spire. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let `default` = DateFormatter(fromFormatString: "EEEE, MMMM d, yyyy")
+
+    convenience init(fromFormatString string: String) {
+        self.init()
+        self.dateFormat = string
+    }
+}
